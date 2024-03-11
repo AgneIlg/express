9c90ec742f59b9bf4cb222ef8ba994143cb0ba18
AgneIlg <agneilginyte@yahoo.com>
2024-03-11 20:44:49 +0200

    inner pages

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import express from 'express';
 import { PageHome } from './pages/PageHome.js';
 import { Page404 } from './pages/Page404.js';
 import { PageAbout } from './pages/PageAbout.js';
+import { PageServicesList } from './pages/PageServicesList.js';
+import { PageServiceInner } from './pages/PageServiceInner.js';
 
 const app = express();
 const port = 4600;
 
-app.use(express.static('static'))
+app.use(express.static('static'));
 
 
 app.get('/', (req, res) => {
@@ -20,6 +22,19 @@ app.get('/about', (req, res) => {
   const page = new PageAbout();
   res.send(page.render());
 });
+
+app.get('/services', (req, res) => {
+  const page = new PageServicesList();
+  res.send(page.render());
+});
+
+app.get('/services/:serviceId', (req, res) => {
+  const page = new PageServiceInner(req.params);
+  res.send(page.render());
+});
+
+
+
 
 app.use((req, res, next) => {
     const page = new Page404();
